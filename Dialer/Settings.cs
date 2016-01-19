3742ef8749c30ec0ad0f0ad23bba24af6ccbe5b9
David Syengo <uboy96@gmail.com>
2016-01-19 17:21:56 +0300

    added settings XML and settings GUI finalised.

---
+++ Dialer/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Dialer
+{
+    [XmlRoot("setting")]
+    public class Settings
+    {
+        
+    }
+}
