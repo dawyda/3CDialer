5ad11d6e95625c6646f97e621f76fc58c4f61a72
David Syengo <uboy96@gmail.com>
2016-02-11 18:25:40 +0300

    added login win for server gui

---
+++ Dialer/LoginWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Dialer
+{
+	/// <summary>
+	/// Interaction logic for LoginWindow.xaml
+	/// </summary>
+	public partial class LoginWindow : Window
+	{
+		public LoginWindow()
+		{
+			this.InitializeComponent();
+			
+			// Insert code required on object creation below this point.
+		}
+
+		private void DoLogin(object sender, System.Windows.RoutedEventArgs e)
+		{
+			// TODO: Add event handler implementation here.
+		}
+	}
+}
